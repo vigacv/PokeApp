b41b4a01b048557fc6e338f7b37e8ddfed13b205
NyoByte <20173527@aloe.ulima.edu.pe>
2021-11-16 07:43:08 -0500

    Aumento del saveUser para Favoritos

--- app/src/main/java/pe/edu/ulima/pm/pokeapp/HomeActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/pokeapp/HomeActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
@@ -28,13 +31,24 @@ class HomeActivity: AppCompatActivity() {
             }
         })
 
+        val eteName = findViewById<EditText>(R.id.eteName)
+
         findViewById<Button>(R.id.btnContinuar).setOnClickListener{
-            changeActivity("PokemonsFragment")
-            saveUser(findViewById<EditText>(R.id.eteName).text.toString())
+            if ( eteName.text!!.isBlank()){
+                Toast.makeText(this, "Debe ingresar su nombre", Toast.LENGTH_LONG).show()
+            }else {
+                changeActivity("PokemonsFragment")
+                saveUser(findViewById<EditText>(R.id.eteName).text.toString())
+            }
         }
 
         findViewById<Button>(R.id.btnFavoritos).setOnClickListener{
-            changeActivity("PokemonsFavoriteFragment")
+            if ( eteName.text!!.isBlank()){
+                Toast.makeText(this, "Debe ingresar su nombre", Toast.LENGTH_LONG).show()
+            }else {
+                changeActivity("PokemonsFavoriteFragment")
+                saveUser(findViewById<EditText>(R.id.eteName).text.toString())
+            }
         }
     }
 
